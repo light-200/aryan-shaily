e233a371a1a131d38eee1c28cb07f3451124789d
light-200 <44830619+light-200@users.noreply.github.com>
2023-05-30 23:32:57 +0530

    feat/imageViewer added 👷🏾

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -1,6 +1,6 @@
+import ImageViewer from "@/components/ImageViewer";
 import PageTitle from "@/components/PageTitle";
 import PageWrapper from "@/components/PageWrapper";
-import Image from "next/image";
 
 export default function Random() {
   return (
@@ -39,23 +39,7 @@ export default function Random() {
 
       <div className="row-start-2 mt-14 lg:row-start-2 lg:mt-10 xl:text-3xl lg:text-2xl uppercase lg:col-start-5 col-span-3 lg:col-span-2 lg:row-span-4 flex flex-col gap-2 lg:gap-6">
         <h3 className="font-bold">Favorite Images</h3>
-        <div className="flex gap-2 lg:overflow-hidden">
-          <Image
-            src="https://picsum.photos/500/500"
-            width={300}
-            height={300}
-            alt={"images"}
-            className="aspect-square object-center object-cover"
-          />
-          <div className="flex flex-col justify-end gap-2">
-            <span className="w-14 h-14 grid place-content-center rounded-full border-2 border-black cursor-pointer select-none hover:bg-gray-200 transition duration-100 active:bg-gray-300">
-              {">"}
-            </span>
-            <span className="w-14 h-14 grid place-content-center rounded-full border-2 border-black cursor-pointer select-none hover:bg-gray-200 transition duration-100 active:bg-gray-300">
-              {"<"}
-            </span>
-          </div>
-        </div>
+        <ImageViewer />
       </div>
 
       <PageTitle classes={"row-start-5 mt-5"}>RANDOM</PageTitle>
